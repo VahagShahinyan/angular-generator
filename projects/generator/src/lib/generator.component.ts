3b988cc58d930bb60e336179ae30a2a13c71cdc0
vahag <vahag1@mail.ru>
2022-01-07 19:45:10 +0400

    - init

---
+++ projects/generator/src/lib/generator.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-generator',
+  template: `
+    <p>
+      generator works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class GeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
